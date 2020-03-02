dc537b3dc8bcbdb034cb539557c54093d6d1aa5b
rfinavia <ronak.finavia@hypaship.com>
2020-03-02 21:09:05 +0000

    1.0

---
+++ test1.py
@@ -0,0 +1,9 @@
+def main():
+    A = 5
+    B = 10
+    C = A + B
+    print(C)
+
+
+if __name__ == '__main__':
+    main()
---
+++ test3.py
@@ -0,0 +1,9 @@
+def main():
+    A = 15
+    B = 15
+    C = A + B
+    print(C)
+
+
+if __name__ == '__main__':
+    main()
